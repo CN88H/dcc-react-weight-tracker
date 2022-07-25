5a87dfa33dfcfe589196bb234d343bae9c957589
CN88H <cheenou88@gmail.com>
2022-07-25 09:36:48 -0500

    Completed working newEntry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '7-23-2022'}, {weight: 177, date: '7-23-2022'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
 
     </div>
   );
